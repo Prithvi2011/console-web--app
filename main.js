fa46b4cdeabda003b6501f0ef2516e670b2be29e
Prithvi2011 <76046770+Prithvi2011@users.noreply.github.com>
2022-06-29 17:15:32 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var my_first_console_project="My first console project";
+console.log(my_first_console_project);
+var first_name="Prithvi";
+
+var last_name="Mantur";
+
+var full_name= first_name.concat(last_name);
+console.log(full_name);
+
+var power_eg_1 = Math.pow(5,1);
+console.log(power_eg_1);
